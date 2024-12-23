df78c2dc383f9605afbaa38782d426a6d11bf384
SV Curious Cat <svcuriouscat@protonmail.com>
2024-12-23 16:45:43 -0500

    update content, remove "money" page

--- webgen.py
+++ webgen.py
@@ -39,7 +39,7 @@ def generateNavigation():
     links = []
     # links.append(getWebPageLink("/captains-log/", "Captain’s log"))
     links.append(getWebPageLink("/inspirations/", "Inspirations"))
-    links.append(getWebPageLink("/money/", "Money"))
+    # links.append(getWebPageLink("/money/", "Money"))
     links.append(getWebPageLink("/software/", "Software"))
     links.append(getWebPageLink("/gallery/", "Gallery"))
     links.append(getWebPageLink("/systems/", "Systems"))
